1b3933b67c8e160d27bdb82b694ee0553447fe16
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2022-02-19 13:24:04 +0530

    react icons added

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { ImBooks } from "react-icons/im";
+import { RiChatSmileFill } from "react-icons/ri";
 
 export default function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <div className="nav">
+      <div className="nav__logo">logo</div>
+      <div className="nav__links">
+        <div>
+          <div>
+            <ImBooks />
+          </div>
+          <p>Books</p>
+        </div>
+        <div>
+          <div>
+            <RiChatSmileFill />
+          </div>
+          <p>Connect</p>
+        </div>
+      </div>
+      <div className="nav__user"></div>
+    </div>
+  );
 }
